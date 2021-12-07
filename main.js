0c401610ac6d465fb64621f2d1cf946ccbeed759
Jay-Gadhiya <83581737+Jay-Gadhiya@users.noreply.github.com>
2021-12-07 10:27:05 +0530

    FEAT : add title and search for title feacher

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ const addBtn = document.querySelector("#addBtn");
 const userInput = document.querySelector("#addTxt");
 const notesDiv = document.querySelector("#notes");
 const search = document.querySelector("#search");
+const userTtl = document.querySelector('#addTtl');
 
 addBtn.addEventListener("click", addNote);
 search.addEventListener("input", searchNote);
 
 showNotes();
 
+
+
 function addNote(e) {
   // store to local storage
   let userText = userInput.value.toLowerCase();
+  let userTitle = userTtl.value.toLowerCase();
   let notes = localStorage.getItem("notes");
 
   if (notes == null) {
@@ -19,10 +23,16 @@ function addNote(e) {
     notesObj = JSON.parse(notes);
   }
 
-  notesObj.push(userText);
+  let userValObj = {
+    title : userTitle,
+    text : userText
+  }
+
+  notesObj.push(userValObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
 
   userInput.value = "";
+  userTtl.value = "";
   showNotes();
 }
 
@@ -42,8 +52,8 @@ function showNotes() {
     html += `
         <div class="noteCard card my-2 mx-2" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">Note ${index + 1}</h5>
-              <p class="card-text">${element}</p>
+              <h5 class="card-title">${index+1}. ${element.title}</h5>
+              <p class="card-text">${element.text}</p>
               <button id="${index}" onclick ="deleteItems(this.id)" class="btn btn-primary">Delete Note</button>
               </div>
             </div> `;
@@ -79,8 +89,9 @@ function searchNote() {
 
   Array.from(noteCard).forEach(function (element) {
     let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    let cardTitle = element.getElementsByTagName('h5')[0].innerText;
 
-    if (cardTxt.includes(inputVal)) {
+    if (cardTxt.includes(inputVal) || cardTitle.includes(inputVal)) {
       element.style.display = "block";
     } else {
       element.style.display = "none";
